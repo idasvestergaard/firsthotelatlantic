99b1ebe35179a79e45b3b8b390cb17766065b14d
Unknown <eaaidve@students.eaaa.dk>
2019-05-23 10:18:02 +0200

    Småting

--- js/main.js
+++ js/main.js
@@ -25,10 +25,10 @@ function appendPosts(posts) {
 
 
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("theModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("theBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -38,7 +38,7 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
