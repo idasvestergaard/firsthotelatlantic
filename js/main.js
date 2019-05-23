93a17345e0abcacdddd2a0a89d3fdedd2be9b990
Franciska <f.agenskov@gmail.com>
2019-05-23 10:52:54 +0200

    Bootstrap med popup

--- js/main.js
+++ js/main.js
@@ -23,29 +23,12 @@ function appendPosts(posts) {
   }
 }
 
-
-// Get the modal
-var modal = document.getElementById("theModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("theBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
